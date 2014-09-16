fc4dbfd5ae63bf6b99bc051f70d27487a2927945
Omar Qureshi <omar@omarqureshi.net>
2014-09-16 11:42:04 +0100

    New clinician error handling

--- app/scripts/controllers/clinicians.js
+++ app/scripts/controllers/clinicians.js
@@ -1,16 +1,19 @@
 'use strict';
 
 angular.module('buddyClientApp')
-    .controller('NewClinicianCtrl', function($scope, Clinician, Team, CurrentUser) {
+    .controller('NewClinicianCtrl', function($scope, $state, Clinician, Team, CurrentUser) {
         $scope.clinician = {}
-        $scope.clinician.teams = [];
         $scope.teams = Team.query({})
         $scope.current_team = CurrentUser.user().account_ids[0];
         $scope.submit = function() {
             var selectedTeams = _.select($scope.teams, function(team) { return team.selected })
             var teamIds = _.map(selectedTeams, function(team) { return team.id})
-            $scope.clinician.account_ids = teamIds
-            Clinician.save({user: $scope.clinician});
+            Clinician.save({user: $scope.clinician}, function() {
+                $state.go('clinicianAdmin.clinicians')
+            }, function(response) {
+                _.each(response.data, function(value, key) { response.data[key] = value[0] });
+                $scope.errors = response.data;
+            });
         }
     })
     .controller('CliniciansCtrl', function ($scope, Clinician, Team, Page, $modal, $location) {
