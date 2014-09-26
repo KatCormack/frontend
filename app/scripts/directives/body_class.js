b737ebfa39521114f587a613263abb9dbabb694e
Omar Qureshi <omar@omarqureshi.net>
2014-09-26 12:01:57 +0100

    Document the hack

--- app/scripts/directives/body_class.js
+++ app/scripts/directives/body_class.js
@@ -7,6 +7,10 @@ angular.module('buddyClientApp')
             scope: false,
             link: function(scope, element) {
                 scope.$watch('state.current.controller', function() {
+                    // for some reason element.removeClass() was not
+                    // working, as a workaround for now - we are
+                    // removing the entire class attribute, then
+                    // adding it afterwards
                     element.removeAttr('class')
                     element.addClass(scope.state.current.controller);
                 });
